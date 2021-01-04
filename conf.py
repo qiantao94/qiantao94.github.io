8209a36df0f53f5395e0882f7af5c100140be6f4
Salama <qiantao94@gmail.com>
2021-01-04 23:23:16 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "qiantao94/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
